66e3587af6736bb6075465e866ad7cd35e5385c3
Diego Fernando Quevedo Solis <dievfer@Diegos-MacBook-Pro.local>
2024-05-06 03:09:37 +0200

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import MyUserRoute from './routes/MyUserRoute';
+import myUserRoute from './routes/MyUserRoute';
+import { v2 as cloudinary} from 'cloudinary';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!")).catch((error) => console.error('Error connecting to database', error));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -14,7 +22,10 @@ app.get("/health", async (req: Request, res: Response) => {
     res.send({message: 'Health OK!'});
 })
 
-app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+
+
 
 app.listen(8000, () => {
     console.log("Server started on localhost 8000")
